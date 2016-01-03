bc07726a050eb2f0423d9dd01a7c378c7454b83d
ibaltopoulos <ioannis.baltopoulos@gmail.com>
2016-01-03 17:35:02 +0000

    Updated photo rename so that it uses the iPhone filename format to rename the file if the metadata is not present.

--- PhotoRename/Program.cs
+++ PhotoRename/Program.cs
@@ -19,10 +19,10 @@ namespace PhotoRename
             var i = 1;
             foreach (var file in files)
             {
-                Console.WriteLine(string.Format("Processing file {0} of {1}", i, length));
+                Console.WriteLine(string.Format("Processing file {0} of {1}. [{2}]", i, length, Path.GetFileName(file)));
                 var dateTime = GetDateTakenFromImage(file);
                 var newFile = GetNewFilename(dateTime);
-                var output = String.Format("Old: {0}\tNew: {1}", file, newFile);
+                var output = string.Format("Old: {0}\tNew: {1}", file, newFile);
                 //Console.Write(output);
                 if (!File.Exists(newFile))
                 {
@@ -33,10 +33,19 @@ namespace PhotoRename
             
         }
 
+        private static string ToZeroPaddedString(int num)
+        {
+            // Info about using D2. http://stackoverflow.com/questions/13043521/whats-the-difference-between-tostringd2-tostring00
+            var paddedInteger = num.ToString("D2");
+            return paddedInteger;
+        }
+
         private static string GetNewFilename(DateTime dt)
         {
-            var output = string.Format("{0}{1}{2}_{3}{4}{5}", dt.Year, dt.Month,dt.Day, dt.Hour, dt.Minute, dt.Second.ToString("D2"));
-            output = output + ".jpg";
+            var date = string.Format("{0}{1}{2}", ToZeroPaddedString(dt.Year), ToZeroPaddedString(dt.Month), ToZeroPaddedString(dt.Day));
+            var time = string.Format("{0}{1}{2}", ToZeroPaddedString(dt.Hour), ToZeroPaddedString(dt.Minute), ToZeroPaddedString(dt.Second));
+
+            var output = string.Format("{0}_{1}.jpg", date, time);
             return output;
         }
 
@@ -47,10 +56,40 @@ namespace PhotoRename
             using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
             using (Image myImage = Image.FromStream(fs, false, false))
             {
-                PropertyItem propItem = myImage.GetPropertyItem(36867);
-                string dateTaken = r.Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2);
-                return DateTime.Parse(dateTaken);
+                try {
+                    PropertyItem propItem = myImage.GetPropertyItem(36867);
+                    string dateTaken = r.Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2);
+                    return DateTime.Parse(dateTaken);
+                } catch (ArgumentException e)
+                {
+                    return GetDateTakenFromFileName(path);
+                }
             }
+        }
+        // File 25-10-2015, 22 37 37.jpeg
+        private static Regex re = new Regex(@"(\d{2})-(\d{2})-(\d{4}),\s(\d{2})\s(\d{2})\s(\d{2})");
+        
+
+        public static DateTime GetDateTakenFromFileName(string path)
+        {
+            var filename = Path.GetFileName(path);
+            var m = re.Match(filename);
+            
+            var day = GetNextItem(m, 1);
+            var month = GetNextItem(m, 2);
+            var year = GetNextItem(m, 3);
+            var hour = GetNextItem(m, 4);
+            var minute = GetNextItem(m, 5);
+            var second = GetNextItem(m, 6);
+
+            return new DateTime(year, month, day, hour, minute, second);
+            
+        }
+
+        private static int GetNextItem(Match m, int index)
+        {            
+            return int.Parse(m.Groups[index].Value);
+            
         }
     }
 }
